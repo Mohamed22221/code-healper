993e99ff222ef3cbc547ac435d8508d7b1e7f806
Mohamed Hamdy <99207823+Mohamed22221@users.noreply.github.com>
2023-10-19 21:53:37 +0300

    split screen patern

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
-import logo from "./logo.svg";
+
 import "./App.css";
-import { filterById } from "./helpers/filterById";
+import { ShowExampleSplit } from "./pattern/splitScreen";
 
 function App() {
-  const list = [
-    {
-      id: 1,
-      name: "Steve",
-      email: "steve@example.com",
-    },
-    {
-      id: 2,
-      name: "John",
-      email: "john@example.com",
-    },
-    {
-      id: 3,
-      name: "Pamela",
-      email: "pam@example.com",
-    },
-    {
-      id: 4,
-      name: "Liz",
-      email: "liz@example.com",
-    },
-  ];
-
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        {filterById(list, 4).map((item) => {
-          return <p>{item.name}</p>;
-        })}
-      </header>
+    <div >
+      <ShowExampleSplit />
     </div>
   );
 }
---
+++ src/pattern/splitScreen.js
@@ -0,0 +1,36 @@
+import React from "react";
+//handel split screen 
+export const SplitScreen = ({ children, leftWidth, rightWidth }) => {
+  const [leftComponent, rightComponent] = children;
+  return (
+    <div className="md:flex">
+      <div className={`${leftWidth}`}>{leftComponent}</div>
+      <div className={`${rightWidth}`}>{rightComponent}</div>
+    </div>
+  );
+};
+//component left
+export const LeftComponent = ({ title }) => {
+  return (
+    <div className="bg-slate-100">
+      <h2>{title}</h2>
+    </div>
+  );
+};
+//component left
+export const RightComponent = ({ title }) => {
+  return (
+    <div className="bg-slate-400">
+      <h2>{title}</h2>
+    </div>
+  );
+};
+//show result split screen in app page
+export const ShowExampleSplit = () => {
+  return (
+    <SplitScreen leftWidth="md:flex-1" rightWidth="md:flex-2">
+      <LeftComponent title="left" />
+      <RightComponent title="right" />
+    </SplitScreen>
+  );
+};
